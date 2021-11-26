4687448d0c9f02c0d003353b0eb8ab3ab8ca919b
Preeetam2004 <95063683+Preeetam2004@users.noreply.github.com>
2021-11-26 09:15:22 +0530

    Create arithmetic operators.c

---
+++ arithmetic operators.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+int a=6,b=5,c;
+
+c=a+b;
+printf("a+b = %d \n",c);
+c=a-b;
+printf("a-b = %d \n",c);
+c=a*b;
+printf("a*b = %d \n",c);
+c=a-b;
+printf("a/b = %d",c);
+c=a%b;
+printf("the remainder when a is divided by b = %d \n",c);
+return 0;
+}
