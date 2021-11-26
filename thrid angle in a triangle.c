33e6f757820c820149b3120ffcf9cfb74a477060
Preeetam2004 <95063683+Preeetam2004@users.noreply.github.com>
2021-11-26 09:19:47 +0530

    Create thrid angle in a triangle.c

---
+++ thrid angle in a triangle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+ int a,b,c;
+ printf("enter the first angle");
+ scanf("%d",&a);
+ printf("enter the second angel");
+ scanf("%d",&b);
+ c=180-(a+b);
+ printf("the value of third angle is %d",c);
+}
