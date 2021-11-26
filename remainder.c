8c65bf1669b5f169d479c64cf392adb9c2359379
Preeetam2004 <95063683+Preeetam2004@users.noreply.github.com>
2021-11-26 09:31:36 +0530

    Create remainder.c

---
+++ remainder.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    
+    int a=8,b=3,c,remainder;
+    
+    c=a/b;
+    printf("%d",c);
+    
+    remainder=a-a/b;
+    printf("\n remainder when a is divided by b is %d",remainder);
+    
+    
+}
